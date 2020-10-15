9df98a6fc65185f75d3a808cb23dfdf4e4249379
Patricia <pattricianunes@gmail.com>
2020-10-15 13:44:07 +0200

    ✨ Add Loader in storybook

---
+++ src/stories/Loader.stories.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
+import Loader from "../components/Loader";
+
+export default {
+  title: "Loader",
+  component: Loader,
+};
+
+export const Default = () => <Loader />;
