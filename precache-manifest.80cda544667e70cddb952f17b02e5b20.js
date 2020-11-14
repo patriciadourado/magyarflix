deea488046614706254bfe922abb5de2ae585856
Patricia <pattricianunes@gmail.com>
2020-11-14 23:42:13 +0100

    Updates

---
+++ precache-manifest.80cda544667e70cddb952f17b02e5b20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef6ef66372b6897084a187758e75504",
+    "url": "/magyarflix/index.html"
+  },
+  {
+    "revision": "5a92e1f561f91816f995",
+    "url": "/magyarflix/static/js/2.6cf78802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/magyarflix/static/js/2.6cf78802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee89652411bd25a9c5c3",
+    "url": "/magyarflix/static/js/main.af931811.chunk.js"
+  },
+  {
+    "revision": "be035d40e9289f8cc4c2",
+    "url": "/magyarflix/static/js/runtime-main.cecaf8bd.js"
+  },
+  {
+    "revision": "3669c20582132277504977fde245c1d5",
+    "url": "/magyarflix/static/media/MagyarFlix.3669c205.png"
+  },
+  {
+    "revision": "4274f57d99cab4cbe185f9768a72ed2c",
+    "url": "/magyarflix/static/media/arrow.4274f57d.svg"
+  },
+  {
+    "revision": "4abc4fb1836e92c74b83f9f835b3c031",
+    "url": "/magyarflix/static/media/best-of-buda.4abc4fb1.png"
+  },
+  {
+    "revision": "919e7c1bd51700cb047216f9fc916987",
+    "url": "/magyarflix/static/media/rubik.919e7c1b.gif"
+  }
+]);
